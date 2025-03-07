8440d6ffdd13479a9b4e75809905e36469c29d38
Honor Everhart <honoreverhart@gmail.com>
2025-03-06 17:47:19 -0700

    home page

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -1,9 +1,14 @@
-export default function HomePage(){
-    return(
-        <div>
-        <h1>Welcome to Endurance Peak!</h1>
-        <h3>Who we are</h3>
-        <p>Endurance Peak is a company striving to help each individual</p>
-        </div>
-    )
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to Endurance Peak!</h1>
+      <h3>Who we are</h3>
+      <p>
+        Endurance Peak is a company striving to help each individual pursue and
+        accomplish they're fitness goals. Every person has peaks to climb.
+        Through perseverance and endurance you can overcome these mountains!
+        Join us today to conquer your fitness peaks!
+      </p>
+    </div>
+  );
 }
