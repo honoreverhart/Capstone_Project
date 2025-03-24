895b6e87f3ed8b9641895e94bb4769f1d12d2219
Honor Everhart <honoreverhart@gmail.com>
2025-03-24 14:56:42 -0600

    Working on photo slideshow

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -1,8 +1,32 @@
 import { useNavigate } from "react-router-dom";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 
 export default function HomePage() {
+  const [slideIndex, setSlideIndex] = useState(1);
   const navigate = useNavigate();
+
+
+  useEffect(()=>{
+    showSlides()
+  },[slideIndex])
+
+
+  function plusSlides(n) {
+    setSlideIndex(prevIndex => {
+      const newIndex = prevIndex + n;
+      return newIndex > 3 ? 1 : newIndex < 1 ? 3 : newIndex;
+    });
+  }
+
+  function showSlides(n) {
+    const slides = document.getElementsByClassName("mySlides");
+    const dots = document.getElementsByClassName("dot");
+  
+    Array.from(slides).forEach(slide => slide.style.display = "none");
+    Array.from(dots).forEach(dot => dot.className = dot.className.replace(" active", ""));
+  
+    slides[slideIndex - 1].style.display = "block";
+  }
 
   return (
     <div>
@@ -22,23 +46,52 @@ export default function HomePage() {
       </div>
 
       <div className="welcome">
-      <h1>Welcome to Endurance Peak!</h1>
+        <h1>Welcome to Endurance Peak!</h1>
       </div>
-      <div className="fitness">
-      <img className="homepage_img" src="/pictures/circlefitness.jpg" alt="fitness" />
+
+      <div className="slideshow-container">
+        <div className="fitness mySlides">
+          <div className="numbertext">1 / 3</div>
+          <img src="/pictures/circlefitness.jpg" />
+          <div className="text">Stay Strong</div>
+        </div>
+
+        <div className="fitness mySlides">
+          <div className="numbertext">2 / 3</div>
+          <img src="/pictures/bestrong.jpg" />
+          <div className="text">You Matter</div>
+        </div>
+
+        <div className="fitness mySlides">
+          <div className="numbertext">3 / 3</div>
+          <img src="/pictures/healthylifestyle.jpg" />
+          <div className="text">Your Way</div>
+        </div>
+
+        <a className="prev" onClick={()=> plusSlides(-1)}>
+          &#10094;
+        </a>
+        <a className="next" onClick={()=> plusSlides(1)}>
+          &#10095;
+        </a>
       </div>
+      <br></br>
+
       <div className="who_we_are">
-      <h3>Who we are</h3>
-      <p>
-        Endurance Peak is a company striving to help each individual pursue and
-        accomplish they're fitness goals. Every person has peaks to climb.
-        Through perseverance and endurance you can overcome these mountains!
-        Join us today to conquer your fitness peaks!
-        <br></br>
-        <button className="sign_up_button" onClick={() => navigate("/register")}>
-          Sign Up Today!
-        </button>
-      </p>
+        <h3>Who we are</h3>
+        <p>
+          Endurance Peak is a company striving to help each individual pursue
+          and accomplish they're fitness goals. Every person has peaks to climb.
+          Through perseverance and endurance you can overcome these mountains!
+          Join us today to conquer your fitness peaks!
+          <br></br>
+          <button
+            className="sign_up_button"
+            onClick={() => navigate("/register")}
+          >
+            Sign Up Today!
+          </button>
+        </p>
       </div>
     </div>
   );
