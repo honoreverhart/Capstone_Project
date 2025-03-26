1894ac3d6538442ab75cc2e096068f91bf956f38
Honor Everhart <honoreverhart@gmail.com>
2025-03-25 18:34:27 -0600

    build

--- server/index.js
+++ server/index.js
@@ -16,16 +16,14 @@ app.use(express.json());
 const cors = require("cors");
 
 const path = require("path");
-app.get("/", (req, res) =>
-  res.sendFile(
-    path.join(__dirname, "../client/Capstone_Project/dist/index.html")
-  )
-);
+
 app.use(cors());
 app.use(
-  "/assets",
-  express.static(path.join(__dirname, "../client/Capstone_Project/dist/assets"))
+  (express.static(path.join(__dirname, '../client/dist')))
 );
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist", 'index.html'));
+});
 
 //middleware
 const isLoggedIn = async (req, res, next) => {
