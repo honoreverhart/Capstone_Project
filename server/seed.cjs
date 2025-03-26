ce8dc3bea5c950d243947ec355e8dbe5400a6693
Honor Everhart <honoreverhart@gmail.com>
2025-03-25 19:32:18 -0600

    fixed seed

--- server/seed.cjs
+++ server/seed.cjs
@@ -20,51 +20,49 @@ async function builtdb() {
     await createTables();
     console.log("tables created");
 
-    const [moe, lucy, ethyl, curly, run, bench, backsquat] = await Promise.all([
-      createUser({
-        first_name: "Moe",
-        last_name: "Black",
-        email: "moe@gmail.com",
-        username: "moe",
-        password: "m_pw",
-        role: "client"
-      }),
-      createUser({
-        first_name: "Lucy",
-        last_name: "Green",
-        email: "lucy@gmail.com",
-        username: "lucy",
-        password: "l_pw",
-        role: "trainer"
-      }),
-      createUser({
-        first_name: "Ethyl",
-        last_name: "Red",
-        email: "ethyl@gmail.com",
-        username: "ethyl",
-        password: "e_pw",
-        role: "trainer"
-      }),
-      createUser({
-        first_name: "Curly",
-        last_name: "Purple",
-        email: "curly@gmail.com",
-        username: "curly",
-        password: "c_pw",
-        role: "trainer"
-      }),
-      createWorkout({ name: "run", description: "run 4 miles" }),
-      createWorkout({
-        name: "bench",
-        description:
-          "bench 55% of your one rep max every two minutes for ten minutes",
-      }),
-      createWorkout({ name: "backsquat", description: "backsquat bodyweight" }),
-    ]).then(()=> {console.log("feed done!!")});
-    
-    
-    return;
-    
+    await createUser({
+      first_name: "Moe",
+      last_name: "Black",
+      email: "moe@gmail.com",
+      username: "moe",
+      password: "m_pw",
+      role: "client",
+    });
+    await createUser({
+      first_name: "Lucy",
+      last_name: "Green",
+      email: "lucy@gmail.com",
+      username: "lucy",
+      password: "l_pw",
+      role: "trainer",
+    });
+    await createUser({
+      first_name: "Ethyl",
+      last_name: "Red",
+      email: "ethyl@gmail.com",
+      username: "ethyl",
+      password: "e_pw",
+      role: "trainer",
+    });
+    await createUser({
+      first_name: "Curly",
+      last_name: "Purple",
+      email: "curly@gmail.com",
+      username: "curly",
+      password: "c_pw",
+      role: "trainer",
+    });
+    await createWorkout({ name: "run", description: "run 4 miles" });
+    await createWorkout({
+      name: "bench",
+      description:
+        "bench 55% of your one rep max every two minutes for ten minutes",
+    });
+    await createWorkout({ name: "backsquat", description: "backsquat bodyweight" });
+
+    console.log("feed done!")
+    client.end();
+
   } catch (error) {
     throw error;
   }
